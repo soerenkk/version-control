0d2720baa362f6ce67000782273839d5f85ee964
soerenkk <soerenkkjeldsen@hotmail.com>
2014-03-23 01:05:07 +0100

    IP
    show IP

--- app.php
+++ app.php
@@ -12,6 +12,9 @@ The app
 */
 function init()
 {
+	// Show visitor its IP address \\
+	echo "Your IP is = " . $_SERVER['REMOTE_ADDR'];
+	
 	phpinfo();
 	// From version 1.1.0 we decided to use die() to prevent any future errors \\
 	die();
